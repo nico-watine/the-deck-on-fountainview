cf3f66f59ed80ce29c06b52c360b1e70ca6150b8
Nico Watine <nico@studioncreations.com>
2016-11-30 12:34:19 -0600

    magellan.js moved from foundation.js to menu.js
    Adds up to a smaller overall size of JS files

--- js/src/menu.js
+++ js/src/menu.js
@@ -3,6 +3,9 @@
         // SKROLLR.MIN.JS
         // JRESPOND.MIN.JS
 
+        // NEW ::
+        // magellan.js
+
 // -----------------------------------
 
 
@@ -25,5 +28,17 @@ jRes.addFunc({
     breakpoint: 'small',
     enter: function() {
         window.location="/menu-m";
+    }
+});
+
+// INITIALIZE FOUNDATION SCRIPTS (INCLUDES EDITS FOR MAGELLAN ON MENU PAGE) ::
+$(document).foundation({
+    "magellan-expedition": {
+      active_class: 'active', // specify the class used for active sections
+      threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+      destination_threshold: 0, // pixels from the top of destination for it to be considered active
+      throttle_delay: 50, // calculation throttling to increase framerate
+      fixed_top: 0, // top distance in pixels assigend to the fixed element on scroll
+      offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
     }
 });
