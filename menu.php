e9b887a7a645e81931b0017f3470b38641e16caf
Nico Watine <nico@studioncreations.com>
2016-12-02 11:18:49 -0600

    Cleared old entree items

--- menu.php
+++ menu.php
@@ -296,80 +296,18 @@
 
 	<section id="entrees">
 		<div class="row">
-			<div class="small-12 columns section-title">
+			<div class="small-12 columns menu-category-title">
 				<a name="entrees"></a>
-				<h2 data-magellan-destination="entrees">Entrees</h2>
-			</div>
-		</div>
-		<div class="row">
-			<div class="small-12 columns section-detail">
-				<h5>-Split Entree for $2.00-</h5>
+				<p data-magellan-destination="entrees">Entrees</p>
+				<h2>Owner's Box</h2>
 			</div>
 		</div>
 		<div class="row item-row">
 			<div class="medium-6 columns">
-				<ul class="no-bullet">
-					<li>Crispy Chicken Tender Basket <span>- $8.99</span></li>
-						<ul class="description">
-							<li>Regular with honey mustard or buffalo style, tossed in spicy sauce and served with fries</li>
-						</ul>
-					<li>Chicken Fried Chicken <span>- $9.99</span></li>
-						<ul class="description">
-							<li>10oz chicken breast, breaded and fried to a gold brown. Topped with white country gravy and served with mashed potatoes &amp; grilled vegetables</li>
-						</ul>
-					<li>Chicken Fried Steak <span>- $9.99</span></li>
-						<ul class="description">
-							<li>10oz chicken fried steak, fried to a gold brown. Topped with your choice of brown or white country gravy and served with mashed potatoes and grilled vegetables</li>
-						</ul>
-					<li>Pork Chops <span>- $9.99</span></li>
-						<ul class="description">
-							<li>Two grilled or blackened Prime pork chops, served with mashed potatoes and grilled vegetables</li>
-						</ul>
-					<li><img src="/img/menu/pork-chops.jpg" alt="Pork Chops"></li>
-					<li>Sandwhiches <span>- $7.99</span></li>
-						<ul class="description">
-							<li>Chicken Sandwhich - Blackened, Buffalo, Grilled or Fried</li>
-							<li>Meatball Sub | Club Sandwhich | Beef or Chicken Philly</li>
-						</ul>
-				</ul>
+
 			</div>
 			<div class="medium-6 columns">
-				<ul class="no-bullet">
-					<li>Wraps <span>- $7.99</span></li>
-						<ul class="description">
-							<li>Ham | Turkey | Caesar</li>
-							<li>Grilled Turkey | Ham &amp; Cheese</li>
-						</ul>
-					<li>Chicken Breast <span>- $9.99</span></li>
-						<ul class="description">
-							<li>8oz all-natural chicken breast, blacked or grilled and served up with mashed potatoes and grilled vegetables</li>
-						</ul>
-					<li>Tostadas <span>- $7.99</span></li>
-						<ul class="description">
-							<li>Three crispy Tostadas topped with seasoned ground beef, shredded lettuce, tomatoes, avocadoes &amp; Mexican cheese</li>
-						</ul>
-					<li>Chicken or Shrimp Alfredo <span>- $9.99</span></li>
-						<ul class="description">
-							<li>Generous portion of sliced chicken breast or Gulf Tiger Shrimp, served over fettuccini and topped with home-made alfredo sauce and parmesan cheese. Served with garlic bread and mixed vegetables</li>
-						</ul>
-					<li>Queso Pasta <span>- $9.99</span></li>
-						<ul class="description">
-							<li>Penne pasta topped with our secret house blend of spicy cheese, topped with blackened &amp; sliced chicken breast and served with garlic bread</li>
-						</ul>
-					<li>Deck Tacos <span>- $9.99</span></li>
-						<ul class="description">
-							<li>3 soft or crispy tacos, stuffed with fajita beef or chicken, and served with refried beans &amp; rice</li>
-						</ul>
-					<li>Tilapia <span>- $9.99</span></li>
-						<ul class="description">
-							<li>10oz filet of blacked or grilled tilapia, served with mashed potatoes and grilled vegetables</li>
-							<li>Add Crawfish Etoufee - $2.99</li>
-						</ul>
-					<li>Crawfish <span>- Market Price/lb</span></li>
-						<ul class="description">
-							<li>Add your choice of Sausage or Corn &amp; Potatos - $1.50 each</li>
-						</ul>
-				</ul>
+
 			</div>
 		</div>
 	</section>
