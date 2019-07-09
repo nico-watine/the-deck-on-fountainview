d83a4960c0c38834e9faa2f5c793196d5ffede3c
Nico Watine <nico@studioncreations.com>
2019-07-09 01:55:40 -0500

    Add functions to file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require('gulp');
-gulp.task('default', function() {
-  // place code for your default task here
+var uncss = require('gulp-uncss');
+var nano = require('gulp-cssnano');
+
+gulp.task('default', function () {
+	return gulp.src('css/app.css')
+		.pipe(uncss({
+			html: ['http://nicos-mbp.local:5757/']
+		}))
+		.pipe(nano())
+		.pipe(gulp.dest('./css/opt'));
 });
