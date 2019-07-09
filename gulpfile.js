c3b3f7a4cc7226a9dd2f95dbac9f4d02a3834269
Nico Watine <nico@studioncreations.com>
2019-07-09 02:07:14 -0500

    Set settings for gulp successfuly

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var nano = require('gulp-cssnano');
 gulp.task('default', function () {
 	return gulp.src('css/app.css')
 		.pipe(uncss({
+			ignore: ['.top-bar.expanded', '.top-bar.expanded .title-area'],
 			html: ['http://nicos-mbp.local:5757/']
 		}))
 		.pipe(nano())
