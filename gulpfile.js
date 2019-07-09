f396c9d05844893368fc927879ccb6dd1264bcf3
Nico Watine <nico@studioncreations.com>
2019-07-09 02:13:07 -0500

    Update gulpfile so location.php works

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('default', function () {
 	return gulp.src('css/app.css')
 		.pipe(uncss({
 			ignore: ['.top-bar.expanded', '.top-bar.expanded .title-area'],
-			html: ['http://nicos-mbp.local:5757/']
+			html: ['http://nicos-mbp.local:5757/', 'http://nicos-mbp.local:5757/events.php', 'http://nicos-mbp.local:5757/menu.php', 'http://nicos-mbp.local:5757/location.php']
 		}))
 		.pipe(nano())
 		.pipe(gulp.dest('./css/opt'));
