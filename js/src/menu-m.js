eed179b3d60c8c181d00b6d14e6d0b4df8a137d3
Nico Watine <nico@studioncreations.com>
2019-07-09 20:55:33 -0500

    Remove jrespond usage

--- js/src/menu-m.js
+++ js/src/menu-m.js
@@ -3,25 +3,9 @@
 // @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.js";
 // @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.accordion.js";
 // @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.topbar.js";
-// @codekit-prepend "../../bower_components/jrespond/js/jRespond.min.js";
 
 $(document).foundation({
 	accordion: {
 		multi_expand: true
-	}
-});
-
-var jRes = jRespond([
-	{
-		label: 'medium',
-		enter: 641,
-		exit: 10000
-	}
-]);
-
-jRes.addFunc({
-	breakpoint: 'medium',
-	enter: function() {
-		window.location="/menu";
 	}
 });
