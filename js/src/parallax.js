03102593fbcfe378e7dec3ecb8f7b5644b461748
Nico Watine <nico@studioncreations.com>
2019-07-12 07:56:37 -0500

    Remove redundant conditional mobile statement

--- js/src/parallax.js
+++ js/src/parallax.js
@@ -1,8 +1,6 @@
 // @codekit-prepend "../../bower_components/skrollr/dist/skrollr.min.js";
 
-if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
-	skrollr.init({
-		forceHeight: false,
-		smoothScrolling: true
-	});
-}
+skrollr.init({
+	forceHeight: false,
+	smoothScrolling: true
+});
