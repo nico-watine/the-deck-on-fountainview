ac09798980c012dfbfb409e793a767173e07f17e
Nico Watine <nico@studioncreations.com>
2019-07-12 08:14:19 -0500

    Correctly import both files

--- js/src/foundation.js
+++ js/src/foundation.js
@@ -1,7 +1,5 @@
-// IMPORTS
-	// PREPENDS::
-		// foundation.js
-		// topbar.js
+// @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.js";
+// @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.topbar.js";
 
 // -----------------------------------
 
