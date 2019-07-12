46c0c010d0c96f4d50ca4b97cce15fc88479cadc
Nico Watine <nico@studioncreations.com>
2019-07-12 08:01:16 -0500

    Remove foundation.js from file and import separate

--- js/src/menu-m.js
+++ js/src/menu-m.js
@@ -1,8 +1,4 @@
-// IMPORTS
-
-// @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.js";
 // @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.accordion.js";
-// @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.topbar.js";
 
 $(document).foundation({
 	accordion: {
--- menu-m.php
+++ menu-m.php
@@ -406,6 +406,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/vendor/jquery/2-1-4/jquery-min.js"><\/script>')</script>
+<script src="<?php autoVer('/js/foundation-min.js'); ?>"></script>
 <script src="<?php autoVer('/js/menu-m-min.js'); ?>"></script>
 <script>
 	window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
