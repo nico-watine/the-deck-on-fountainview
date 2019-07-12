0f86d501b5611bff059deb0ae630a704851ef073
Nico Watine <nico@studioncreations.com>
2019-07-12 08:14:44 -0500

    Remove annotations

--- js/src/foundation.js
+++ js/src/foundation.js
@@ -1,7 +1,4 @@
 // @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.js";
 // @codekit-prepend "../../bower_components/foundation/js/foundation/foundation.topbar.js";
 
-// -----------------------------------
-
-// INITIALIZE FOUNDATION SCRIPTS
 $(document).foundation();
